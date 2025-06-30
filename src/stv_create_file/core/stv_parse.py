263e96f396ab5a4d677e7583ee4800ea32fbedc1
星灿长风v <2452234@st.shou.edu.cn>
2025-06-30 13:13:04 +0800

    Update stv_parse.py
    修复了参数解析异常的问题

--- src/stv_create_file/core/stv_parse.py
+++ src/stv_create_file/core/stv_parse.py
@@ -2,7 +2,7 @@ import argparse
 from sys import argv as sys_argv
 from stv_create_file.mul_lang.change_text import parse_text
 from stv_create_file.utils.GetConfig import start_process
-
+__version__ = "0.0.3"
 
 configer = start_process()
 use_verbose = False
@@ -32,7 +32,26 @@ def arg_check(short_cut: str = '', full_arg: str = '')->bool:
     return False
 
 
+def advance_parse():
+    parser = argparse.ArgumentParser(add_help = False)
+    parser.add_argument('-lic', '--license', action="store_true")
+    parser.add_argument('-V', '--version', action="store_true")
+    parser.add_argument('-sl', '--set-language', type=str, choices=['chinese', 'english'])
+    args, _ = parser.parse_known_args()
+    if args.version:
+        print(f"Project Version: {__version__}")
+        exit(0)
+    if args.license:
+        print("Project License: MIT")
+        exit(0)
+    if args.set_language:
+        configer.change(key = "lang", value = "chinese" if args.set_language == "chinese" else "english")
+        print("Language changed successfully." if (configer.lang == "english" or configer.lang == "en-uk") else "语言已更改成功")
+        exit(0)
+
+
 def stv_parse():
+    advance_parse()
     text = parse_text(function_name="stv_parse")
     parser = argparse.ArgumentParser(description=text["description"])
     parser.add_argument('path', nargs='+', help=text["path"])
@@ -49,12 +68,12 @@ def stv_parse():
     parser.add_argument('-lic', '--license', action="store_true", help=text["license"])
     parser.add_argument('-v', '--verbose', action="store_true", help=text["verbose"])
     parser.add_argument('-D', '--Debug', action="store_true", help=argparse.SUPPRESS)
+
+    parser.add_argument('-sl', '--set-language', type=str,
+                        choices=['chinese', 'english'],help=text["lang"])
     args = parser.parse_args()
 
     if use_verbose:
         args.verbose = True
-    if args.version:
-        print("Project Version: 0.0.1")
-    if args.license:
-        print("Project License: MIT")
+
     return args
