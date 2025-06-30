fb2eb45cd07f0d34be298067e9396fef215df9eb
星灿长风v <2452234@st.shou.edu.cn>
2025-06-30 13:33:37 +0800

    Update GetConfig.py
    新增了更新配置的方法

--- src/stv_create_file/utils/GetConfig.py
+++ src/stv_create_file/utils/GetConfig.py
@@ -8,7 +8,11 @@ config_path = os.path.join(home, ".stv_project/config.json")
 
 
 class ConfigProcessor:
-    def load(self, path: str) -> dict:
+    def load(self, path: str, depth: int = 0) -> dict:
+        depth = 1 if not depth else depth
+        if depth > 3:
+            print("Your config file is broken, please delete it and restart the program")
+            exit(-1)
         if os.path.exists(path):
             with open(path, "r", encoding="utf-8") as f:
                 content = json.load(f)
@@ -16,7 +20,12 @@ class ConfigProcessor:
                     try:
                         os.remove(path)
                         self.initialize()
-                        self.load(path)
+                        """
+                        实际上在此处并不会导致递归过多
+                        因为我们能重置文件以打破递归
+                        并且我们有深度限制
+                        """
+                        self.load(path, depth=depth+1)
                     except PermissionError:
                         print("Permission denied")
                 else:
@@ -24,13 +33,13 @@ class ConfigProcessor:
         return {}
 
 
-    def parse(self, args: str) -> bool or None or str:
+    def parse(self, args: str) -> bool or str:
         if self.config:
             try:
                 return self.config[args]
             except KeyError:
                 print(f"No such key: {args}")
-        return None
+        return ''
 
     def __str__(self):
         return f"ConfigPath: {self.path}\nConfigText: {self.config}"
@@ -46,11 +55,20 @@ class ConfigProcessor:
         if not os.path.exists(self.path):
             os.makedirs(os.path.dirname(self.path), exist_ok=True)
             open(self.path, "w", encoding="utf-8").close()
-        with open(self.path, "r+", encoding="utf-8") as f:
+        with (open(self.path, "r+", encoding="utf-8") as f):
             if not f.read():
                 print("Initializing config file...")
-                content = {"lang": "zh-cn","verbose": True,} if is_ch() else {"lang": "en-uk","verbose": True,}
+                content = {"lang": "zh-cn","verbose": False} \
+                            if is_ch() \
+                            else \
+                          {"lang": "en-uk","verbose": False,}
                 self.save(content, f)
+
+    def re_parse(self):
+        self.initialize()
+        self.config = self.load(self.path)
+        self.verbose = self.parse("verbose")
+        self.lang = self.parse("lang")
 
     @staticmethod
     def save(content: dict or json, ptr: str or TextIO = None, path = ""):
@@ -60,11 +78,13 @@ class ConfigProcessor:
         else:
             json.dump(content, ptr, indent=4, ensure_ascii=False)
 
-    def change(self, key: str = "", value: bool = False):
+    def change(self, key: str = "", value: bool or str = False):
         if not key or not value:
             raise ValueError("Key and value must be provided")
         self.config[key] = value
         self.save(self.config, path = self.path)
+        self.re_parse()
+
 
 def start_process()->None or ConfigProcessor:
     loader = ConfigProcessor(config_path)
